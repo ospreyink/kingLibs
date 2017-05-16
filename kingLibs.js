27e29fa64bbcb0a3440cdd43cbf8fd6c4a81b251
Roads <m14stratus@gmail.com>
2017-05-16 12:24:11 -0600

    Adding King Libs to repository

---
+++ kingLibs.js
@@ -0,0 +1,24 @@
+
+
+// Random Words
+
+var activity = ["curling", "playing dress up", "fox hunting", "bowling", "masturbating", "drinking tea", "coding", "painting", "starting fires", "fishing"]
+
+var noun = ["hockey stick", "lunch box", "Corvette", "puppy", "bowl of soup", "wiener schnitzel", "saxophone", "cartoon", "Tupperware", "snake"]
+
+var place = ["McDonalds", "WalMart", "Salt Lake City", "the bathroom", "Lake Titicaca", "the North Pole", "the bottom of the ocean", "the Moon", "Trump's pants", "Hell"]
+
+var nounPl = ["frogs", "websites", "shoes", "B-52s", "Post It notes", "fingernails", "beer bottles", "birds", "car seats", "library books"]
+
+var adjective = ["witty", "green", "bumpy", "slippery", "bright", "furry", "tight", "retarded", "difficult", "wet"]
+
+var emotion = ["delighted", "horny", "devious", "dim-witted", "hungover", "gleeful", "pitiful", "envious", "ashamed", "indignant"]
+
+var verbPastTense = ["hopped", "conjured", "joked", "drank", "ate", "poured", "farted", "moisturized", "bathed", "snowboarded"]
+
+
+// Input variables
+
+var adjective1 = ["test"]
+
+var randomAdjective = adjective[Math.floor(Math.random() * adjective.length)];
